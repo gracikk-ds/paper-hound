ca3ea446e2bce2a7afe3849225fa04d238170d4b
Aleksandr Gordeev <asgordeev@MacBook-Air-Aleksandr.local>
2026-01-13 17:35:45 +0300

    updated google api deps

--- src/settings.py
+++ src/settings.py
@@ -20,14 +20,13 @@ class Settings(BaseSettings):
     api_version: str = Field("0.1.0", description="The version of the API")
 
     vector_store_collection: str = Field("arxiv_papers", description="The collection name for the vector store")
-    vector_store_vector_size: int = Field(2560, description="The size of the vector for the vector store")
+    vector_store_vector_size: int = Field(3072, description="The size of the vector for the vector store")
     vector_store_distance: str = Field("Cosine", description="The distance for the vector store")
     embedding_service_model_name: str = Field(
-        "Qwen/Qwen3-Embedding-4B",
+        "gemini-embedding-001",
         description="The model name for the embedding service",
     )
-    embedding_service_device: str = Field("cpu", description="The device for the embedding service")
-    embedding_service_batch_size: int = Field(32, description="The batch size for the embedding service")
+    embedding_service_batch_size: int = Field(250, description="The batch size for the embedding service")
 
     notion_token: str = Field(..., description="Notion API token.")
     notion_database_id: str = Field("228f6f75bb0b80babf73d46a6254a459", description="Notion database ID.")
@@ -35,7 +34,6 @@ class Settings(BaseSettings):
         {"Image Editing": "228f6f75bb0b8023a7aeced6e6799a89"},
         description="Notion settings database IDs.",
     )
-    site_reports_dir: str = Field("site/_reports", description="Site reports directory.")
     aws_access_key_id: str = Field(..., description="AWS access key ID.")
     aws_secret_access_key: str = Field(..., description="AWS secret access key.")
     endpoint_url: str = Field(..., description="AWS endpoint URL.")
