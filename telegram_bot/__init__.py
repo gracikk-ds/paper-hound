d5a1705f621952f45da1599981799ec4f6054aad
Aleksandr Gordeev <asgordeev@MacBook-Air-Aleksandr.local>
2026-01-16 09:03:28 +0300

    added bot init file

---
+++ telegram_bot/__init__.py
@@ -0,0 +1,17 @@
+"""Telegram bot module for ArXiv paper discovery."""
+
+from telegram_bot.bot import bot_context, create_bot_application, run_bot, stop_bot
+from telegram_bot.notifications import NotificationService, run_subscription_notifications
+from telegram_bot.subscriptions import Subscription, SubscriptionStore, get_subscription_store
+
+__all__ = [
+    "NotificationService",
+    "Subscription",
+    "SubscriptionStore",
+    "bot_context",
+    "create_bot_application",
+    "get_subscription_store",
+    "run_bot",
+    "run_subscription_notifications",
+    "stop_bot",
+]
